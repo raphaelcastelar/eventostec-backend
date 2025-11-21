405ea6893b58897a2e93c4ac162797e0b00871b9
Raphael Castelar <castelaraphael@gmail.com>
2025-11-21 11:36:42 -0300

    create entity address

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.domain.address;
+
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.rmi.server.UID;
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String  city;
+
+    private String  uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+}
